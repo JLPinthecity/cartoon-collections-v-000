d6f09ff41221f8b931a1ebc62c4f72ba755996af
Joann Pan <joannlpan@gmail.com>
2020-05-02 19:54:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,9 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types = ["cheddar", "gouda", "camembert"]
+
+def find_the_cheese(strings)
+  strings.include?(cheese_types)
+  end
 end
